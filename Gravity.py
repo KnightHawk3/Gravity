117eae164444c3403400848356caabb2e9eafa35
Thomas Gilmore <tom@thedisorganisation.com>
2013-09-05 12:45:20 +1000

    Fixed merge issues. Made restructure work w/ new
    Added user defined central particle, width/height and velocity to
    Declan's pythonlike restructure.

--- Gravity.py
+++ Gravity.py
@@ -16,70 +16,27 @@ import pygame
 import random
 import math
 import sys
-<<<<<<< HEAD
-from pygame.locals import *
-
-FRAME_RATE = 60
-
-clock = pygame.time.Clock()
-
-pygame.init()
-
-#The bit that asks you questions
-
-#how many particles do you want?
-while True:
-    ParticleNumber = raw_input("Enter how many particles to draw: ")
-    try:
-        ParticleNumber = int(ParticleNumber)
-        break
-    except:
-        print "\nWell, now.  Let's try that again."
-
-#How big do you want the screen?
-while True:        
-    Screenx = raw_input ("Enter Screen Width: ")
-    try:
-        Screenx = int(Screenx)
-        break
-    except:
-        print "\nWell, now.  Let's try that again."
-        
-while True:
-    Screeny = raw_input ("Enter Screen Height: ")
-    try:
-        Screeny = int(Screeny)
-        break
-    except:
-        print "\nWell, now.  Let's try that again."
-
-#how big do you want to make the Central Particle
-while True:
-    CentreMass = raw_input ("Enter Central Particle Mass (0 = No Centre): ")
-    try:
-        CentreMass = int(CentreMass)
-        break
-    except:
-        print "\nWell, now.  Let's try that again."
-=======
 
 from pygame.locals import *
 
-SCREEN_HEIGHT = 600
-SCREEN_WIDTH = 800
->>>>>>> 5760df3fc3eb6231a058857ea9fefbdc0c93242d
+while True:
+    SCREEN_WIDTH = raw_input("Enter screen width: ")
+    # Make sure the number supplied is an int otherwise ask again.
+    try:
+        SCREEN_WIDTH = int(SCREEN_WIDTH)
+        break
+    except:
+        print "\nWell, now.  Let's try that again."
 
-# how fast do you want the particles?
-ParticleVelocity = input ("Enter Max Particle Velocity: ") 
-
-<<<<<<< HEAD
-#The bit that that makes the screen
-Surface = pygame.display.set_mode((Screenx, Screeny))
-
-#The bit that makes the particles work
-Particles = []
-=======
->>>>>>> 5760df3fc3eb6231a058857ea9fefbdc0c93242d
+while True:
+    SCREEN_HEIGHT = raw_input("Enter screen height: ")
+    # Make sure the number supplied is an int otherwise ask again.
+    try:
+        SCREEN_HEIGHT = int(SCREEN_HEIGHT)
+        break
+    except:
+        print "\nWell, now.  Let's try that again."
+            
 class Particle:
     def __init__(self, x, y, speedx, speedy, mass):
         self.x = x
@@ -89,89 +46,6 @@ class Particle:
         self.mass = mass
         self.radius = math.sqrt(self.mass)
 
-<<<<<<< HEAD
-#The big Central Particle
-if CentreMass > 0:
-    Particles.append(Particle(Screenx/2, Screeny/2, 0.0, 0.0, CentreMass))
-#The little particles
-for x in range(ParticleNumber):
-    Particles.append(Particle(random.randint(10,Screenx - 10), random.randint(10,Screeny - 10), random.uniform(-ParticleVelocity, ParticleVelocity), random.uniform(-ParticleVelocity, ParticleVelocity), random.uniform(1, 10)))
-    
-#The bit that figures out the complex gravity stuff.
-def Move():
-    for P in Particles:
-        for P2 in Particles:
-            if P != P2:
-                XDiff = P.x - P2.x
-                YDiff = P.y - P2.y
-                Distance = math.sqrt((XDiff ** 2) + (YDiff ** 2))
-
-                if Distance < 10:
-                    Distance = 10
-
-                #F = (G*M*M)/(R**2)
-                Force = 0.125 * (P.mass * P2.mass) / (Distance ** 2)
-                #F = M*A  ->  A = F/M
-                Acceleration = Force / P.mass
-                XComponent = XDiff / Distance
-                YComponent = YDiff / Distance
-                P.speedx -= Acceleration * XComponent
-                P.speedy -= Acceleration * YComponent
-
-    for P in Particles:
-        P.x += P.speedx
-        P.y += P.speedy
-
-#The bit that detects collisions (Duh.)
-def CollisionDetect():
-    for P in Particles:
-        for P2 in Particles:
-            if P != P2:
-                Distance = math.sqrt(((P.x - P2.x) ** 2) + ((P.y - P2.y) ** 2))
-                if Distance < (P.radius + P2.radius):
-                    P.speedx = ((P.mass * P.speedx) + (P2.mass * P2.speedx)) \
-                        / (P.mass + P2.mass)
-                    P.speedy = ((P.mass * P.speedy) + (P2.mass * P2.speedy)) \
-                        / (P.mass + P2.mass)
-                    P.x = ((P.mass * P.x) + (P2.mass * P2.x)) \
-                        / (P.mass + P2.mass)
-                    P.y = ((P.mass * P.y) + (P2.mass * P2.y)) \
-                        / (P.mass + P2.mass)
-                    P.mass += P2.mass
-                    P.radius = math.sqrt(P.mass)
-                    Particles.remove(P2)
-
-#The bit that draws up the screen and the dots
-def Draw():
-    Surface.fill((0, 0, 0))
-    for P in Particles:
-        pygame.draw.circle(Surface,
-                           (255, 255, 255),
-                           (int(P.x),
-                           int(600 - P.y)),
-                           int(round(P.radius)))
-##        Surface.set_at((int(P.x),int(600-P.y)),(255,255,255))
-    pygame.display.flip()
-
-# The bit that lets it quit
-def GetInput():
-    keystate = pygame.key.get_pressed()
-    for event in pygame.event.get():
-        if event.type == QUIT or keystate[K_ESCAPE]:
-            pygame.quit()
-            sys.exit()
-
-#The bit that makes it actually run
-def main():
-    while True:
-        # Limiting Frame Rate to 50 fps
-        time_passed = clock.tick(FRAME_RATE)
-        
-        GetInput()
-        Move()
-        CollisionDetect()
-        Draw()
-=======
 
 class Gravity():
     def __init__(self, screen_width, screen_height):
@@ -195,16 +69,27 @@ class Gravity():
             except:
                 print "\nWell, now.  Let's try that again."
 
+        ParticleVelocity = input("Enter particle max speed: ")
+            
+        while True:
+            CentreSize = raw_input("Enter how large the central particle is: ")
+            # Make sure the number supplied is an int otherwise ask again.
+            try:
+                CentreSize = int(CentreSize)
+                break
+            except:
+                print "\nWell, now.  Let's try that again."
+
         # Create a large particle in the center of the system
-        self.particles.append(Particle(600, 300, 0.0, 0.0, 1000))
+        self.particles.append(Particle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0.0, 0.0, CentreSize))
 
         # Create some random little particles.
         for x in range(ParticleNumber):
-            self.particles.append(Particle(random.randint(10, 1190),
-                                           random.randint(10, 690),
-                                           random.uniform(-1.5, 1.5),
-                                           random.uniform(-1.5, 1.5),
-                                           random.uniform(1, 10)))
+            self.particles.append(Particle(random.randint(10, SCREEN_WIDTH - 10),
+                                           random.randint(10, SCREEN_HEIGHT - 10),
+                                           random.uniform(-ParticleVelocity, ParticleVelocity),
+                                           random.uniform(-ParticleVelocity, ParticleVelocity),
+                                           random.randint(1, 10)))
 
     def gameloop(self):
         while True:
@@ -275,7 +160,6 @@ class Gravity():
                                 int(600 - P.y)),
                                int(round(P.radius)))
         pygame.display.flip()
->>>>>>> 5760df3fc3eb6231a058857ea9fefbdc0c93242d
 
 if __name__ == '__main__':
     game = Gravity(SCREEN_WIDTH, SCREEN_HEIGHT)

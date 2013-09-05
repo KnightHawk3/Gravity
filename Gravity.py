28e6c39593772cd1abbad10b181bd1397a702d4c
Thomas Gilmore <tom@thedisorganisation.com>
2013-09-05 11:29:53 +1000

    Added User-Defined Velocity, Tidied up.
    YAY - CRAPPY ASCII ART!!!
    
    And also I added a couple of features.

--- Gravity.py
+++ Gravity.py
@@ -1,3 +1,17 @@
+#     ___     ____       _     _     _   _____   _______  __ __
+#   /|       ||  \\    // \\   \\   //   I===I   |I---I|  \\ //
+#   ||   __  ||==//   ||===||   \\ //     |||      |||     \Y/
+#   \|___|/  ||  \\   ||   ||    \v/     I===I     |||     |||
+#
+#   A little program that creates little simulation of gravity 
+#   a little solar system. The original program, which did all
+#   the actual "Gravity Simulating" was written by Ian Mallet.
+#                             Enjoy it!
+#   "I've always thought that one of the the great things about
+#    physics is that you can add more digits to any number and
+#          see what happens and nobody can stop you."
+#    -- Randall Monroe
+
 import pygame
 import random
 import math
@@ -10,6 +24,9 @@ clock = pygame.time.Clock()
 
 pygame.init()
 
+#The bit that asks you questions
+
+#how many particles do you want?
 while True:
     ParticleNumber = raw_input("Enter how many particles to draw: ")
     try:
@@ -17,7 +34,8 @@ while True:
         break
     except:
         print "\nWell, now.  Let's try that again."
-        
+
+#How big do you want the screen?
 while True:        
     Screenx = raw_input ("Enter Screen Width: ")
     try:
@@ -34,20 +52,22 @@ while True:
     except:
         print "\nWell, now.  Let's try that again."
 
+#how big do you want to make the Central Particle
 while True:
-    CentreMass = raw_input ("Enter Central Particle Mass: ")
+    CentreMass = raw_input ("Enter Central Particle Mass (0 = No Centre): ")
     try:
         CentreMass = int(CentreMass)
         break
     except:
         print "\nWell, now.  Let's try that again."
 
-Screen_Size = (Screenx, Screeny)
+# how fast do you want the particles?
+ParticleVelocity = input ("Enter Max Particle Velocity: ") 
+
+#The bit that that makes the screen
 Surface = pygame.display.set_mode((Screenx, Screeny))
 
-Particles = []
-
-
+#The bit that makes the particles work
 Particles = []
 class Particle:
     def __init__(self, x, y, speedx, speedy, mass):
@@ -58,12 +78,14 @@ class Particle:
         self.mass = mass
         self.radius = math.sqrt(self.mass)
 
-Particles.append(Particle(Screenx/2, Screeny/2, 0.0, 0.0, CentreMass))
-
+#The big Central Particle
+if CentreMass > 0:
+    Particles.append(Particle(Screenx/2, Screeny/2, 0.0, 0.0, CentreMass))
+#The little particles
 for x in range(ParticleNumber):
-    Particles.append(Particle(random.randint(10,Screenx - 10), random.randint(10,Screeny - 10), random.uniform(-1.5, 1.5), random.uniform(-1.5, 1.5), random.uniform(1, 10)))
+    Particles.append(Particle(random.randint(10,Screenx - 10), random.randint(10,Screeny - 10), random.uniform(-ParticleVelocity, ParticleVelocity), random.uniform(-ParticleVelocity, ParticleVelocity), random.uniform(1, 10)))
     
-
+#The bit that figures out the complex gravity stuff.
 def Move():
     for P in Particles:
         for P2 in Particles:
@@ -88,7 +110,7 @@ def Move():
         P.x += P.speedx
         P.y += P.speedy
 
-
+#The bit that detects collisions (Duh.)
 def CollisionDetect():
     for P in Particles:
         for P2 in Particles:
@@ -107,9 +129,9 @@ def CollisionDetect():
                     P.radius = math.sqrt(P.mass)
                     Particles.remove(P2)
 
-
+#The bit that draws up the screen and the dots
 def Draw():
-    Surface.fill((25, 0, 0))
+    Surface.fill((0, 0, 0))
     for P in Particles:
         pygame.draw.circle(Surface,
                            (255, 255, 255),
@@ -119,7 +141,7 @@ def Draw():
 ##        Surface.set_at((int(P.x),int(600-P.y)),(255,255,255))
     pygame.display.flip()
 
-
+# The bit that lets it quit
 def GetInput():
     keystate = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -127,7 +149,7 @@ def GetInput():
             pygame.quit()
             sys.exit()
 
-
+#The bit that makes it actually run
 def main():
     while True:
         # Limiting Frame Rate to 50 fps
